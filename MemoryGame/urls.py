c123200c6e55fdf8dc0efe5c433022bf780489e9
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-03-12 15:00:25 +0100

    Add new App

--- MemoryGame/urls.py
+++ MemoryGame/urls.py
@@ -21,6 +21,12 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-  
+
+ #Ponizej dodaje sciezki zwiazane a moimi aplikacjami
+
+    #path("Gra", include("Gra.urls")),
+    #path("O_mnie", include("O_mnie.urls")),
+    #path("Statystyki", include("Statystyki.urls")),    
+   
     
 ]
