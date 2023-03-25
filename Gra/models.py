274693b58b422d7037f8c5401d536be750e0ea2e
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-03-25 17:06:25 +0100

    Add submit to form

--- Gra/models.py
+++ Gra/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 
 class NewGame(models.Model):
-    player1 = models.CharField(max_length = 15)
-    player2 = models.CharField(max_length = 15)
-    no_cards = [
-    ("4", "4"),
-    ("6", "6"),
-    ("8", "8"),
+    player1 = models.CharField(max_length = 15, blank=True)
+    no_player2 = [
+    ("player3", "Gram sam"),
+    ("player4", "Gram z komputerem")
     ]
-    number_cards = models.CharField(max_length = 15, choices=no_cards)
+    player2 = models.CharField(max_length = 15, choices=no_player2, blank=True)
+    cards = [
+    ("4cards", 4),
+    ("8cards", 8),
+    ("16cards", 16),
+    ("32cards", 32)
+    ]
+    number_cards = models.CharField(max_length = 15, choices=cards)
     #number_cards = models.DecimalField(default=10 , max_digits=20, decimal_places=0)
     
     
