967f8ed5c29b53840d15e1f87c12c70dffaa362d
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-04-12 15:05:57 +0200

    Load Static and JS

--- Gra/static/gra/new_game_form.js
+++ Gra/static/gra/new_game_form.js
@@ -1,7 +1,14 @@
-$(document).ready(function() {  
+$(document).ready(function() { 
+    $('#div_player2').hide();
     $('#id_player2_select').change(function(){
-        alert( $(this).find("option:selected").attr('value') );    
-        if ($(this).find("option:selected").attr('value')==="player3"){$('#id_player2').show()}
-        else $('#id_player2').hide()  
+        var selected = $(this).find("option:selected").attr('value'); 
+        // alert(selected);
+        // alert(selected==="player3");
+        if (selected==="player3"){
+            $('#div_player2').hide()
+        } else {
+            $('#div_player2').show()
+        }
     });
  });
+
