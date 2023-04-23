bc7b6987a3d5bb7c09a6811726b78071b034b04f
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-04-23 23:19:14 +0200

    Fix JS

--- Gra/static/gra/playboard.js
+++ Gra/static/gra/playboard.js
@@ -1,6 +1,17 @@
-$(document).ready(()=>{
-$('td').each(function(index){var td = $(this).click(function(){
-    console.log(this);
-    this.style.backgroundColor = "white";
-    this.children[0].style.filter = "brightness(1.0)s"
-})})})
+// $(document).ready(()=>{
+// $('td').each(function(index){var td = $(this).click(function(){
+//     console.log(this);
+//     this.style.backgroundColor = "white";
+//     this.children[0].style.filter = "brightness(1.0)s"
+// })})})
+
+$(document).ready(() => {
+    $('td').each(function(index) {
+      const td = $(this);
+      td.click(() => {
+        console.log(this);
+        td.css('background-color', 'white');
+        td.children('img').css('filter', 'brightness(100%)');
+      });
+    });
+  });
