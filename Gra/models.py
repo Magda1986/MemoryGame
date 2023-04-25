f3892d9363a9637560bd1dd0e1a19dee1518201f
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-04-25 22:58:29 +0200

    Disappearing cards JS

--- Gra/models.py
+++ Gra/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from random import shuffle
 
 cards = [
-    (4, "4cards"),
-    (8, "8cards"),
-    (16, "16cards"),
-    (32, "32cards")
+    (4, "4 cards"),
+    (8, "8 cards"),
+    (16, "16 cards"),
+    (32, "32 cards")
     ]
 
 
-def create_playboard(cards_no, row_len=4):
+def create_playboard(cards_no, row_len=8):
     deck = 2 * list(range(int(0.5 * cards_no)))  #tworzymy talie kart
     shuffle(deck)# tasowanie
     return [deck[i: i+row_len] for i in range(0, len(deck), row_len)] #ukladanie kart na planszy
@@ -19,7 +19,7 @@ class NewGame(models.Model):
     player1 = models.CharField(max_length = 15, blank=True)
     player2 = models.CharField(max_length = 15, blank=True)
     number_cards = models.IntegerField(choices=cards)
-    playboard = models.JSONField(blank=True, null=True) #generuje sie jako pusty )
+    playboard = models.JSONField(blank=True, null=True) #generuje sie jako pusty
     
     def save(self, *args, **kwargs):
         self.playboard = create_playboard(self.number_cards)
--- Gra/static/gra/playboard.js
+++ Gra/static/gra/playboard.js
@@ -5,13 +5,79 @@
 //     this.children[0].style.filter = "brightness(1.0)s"
 // })})})
 
+// $(document).ready(() => {
+//     $('td').each(function(index) {
+//       const td = $(this);
+//       td.click(() => {
+//         console.log(this);
+//         td.css('background-color', 'white');
+//         td.children('img').css('filter', 'brightness(100%)');
+//       });
+//     });
+//   });
+
 $(document).ready(() => {
-    $('td').each(function(index) {
-      const td = $(this);
-      td.click(() => {
-        console.log(this);
-        td.css('background-color', 'white');
-        td.children('img').css('filter', 'brightness(100%)');
-      });
+  let firstCard = null;
+  $('td').each(function(index) {
+    const td = $(this);
+    td.click(() => {
+      td.css('background-color', 'white');
+      td.children('img').css('filter', 'brightness(100%)');
+
+      if (!firstCard) {
+        firstCard = td;
+      } else {
+        const firstCardName = firstCard.children('img').attr('alt');
+        const secondCardName = td.children('img').attr('alt');
+        if (firstCardName === secondCardName) {
+          firstCard.fadeOut();
+          td.fadeOut();
+        } else {
+          setTimeout(() => {
+            firstCard.css('background-color', '');
+            firstCard.children('img').css('filter', '');
+            td.css('background-color', '');
+            td.children('img').css('filter', '');
+          }, 4000);
+        }
+        firstCard = null;
+      }
     });
   });
+});
+
+
+// $(document).ready(() => {
+//   let firstCard = null;
+  
+//   $('td').each(function() {
+//     const td = $(this);
+//     td.click(() => {
+//       td.css('background-color', 'white');
+//       td.children('img').css('filter', 'brightness(100%)');
+      
+//       if (firstCard == null) {
+//         firstCard = td;
+//       } else {
+//         const firstCardName = firstCard.children('img').attr('alt');
+//         const secondCardName = td.children('img').attr('alt');
+        
+//         if (firstCardName == secondCardName) {
+//           setTimeout(() => {
+//             firstCard.children('img').fadeOut();
+//             td.children('img').fadeOut();
+//           }, 2000);
+//         } else {
+//           setTimeout(() => {
+//             firstCard.css('background-color', 'blue');
+//             firstCard.children('img').css('filter', 'brightness(50%)');
+//             td.css('background-color', 'blue');
+//             td.children('img').css('filter', 'brightness(50%)');
+//           }, 2000);
+//         }
+        
+//         firstCard = null;
+//       }
+//     });
+//   });
+// });
