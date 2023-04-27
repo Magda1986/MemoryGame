41031d8e63389a29bb37fe14b07ffd0a7752fdc1
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-04-27 22:42:53 +0200

    Fixed JS

--- Gra/static/gra/playboard.js
+++ Gra/static/gra/playboard.js
@@ -1,16 +1,7 @@
 
-// $(document).ready(() => {
-//     $('td').each(function(index) {
-//       const td = $(this);
-//       td.click(() => {
-//         console.log(this);
-//         td.css('background-color', 'white');
-//         td.children('img').css('filter', 'brightness(100%)');
-//       });
-//     });
-//   });
 
-//______________________________________________________________________________________
+
+// --------------------- setne podejście do problemy poniej --------------------------------
 
 
 $(document).ready(() => {
@@ -20,132 +11,109 @@ $(document).ready(() => {
   $('td').click(function() {
     const currentCard = $(this);
     const currentImg = currentCard.children('img');
-    
-    if (firstCard === null) {
-      // Odkrycie pierwszej karty
-      currentCard.css('background-color', 'white');
-      currentImg.css('filter', 'brightness(100%)');
-      firstCard = currentCard;
-    } else if (secondCard === null) {
-      // Odkrycie drugiej karty
-      const firstImg = firstCard.children('img');
-      currentCard.css('background-color', 'white');
-      currentImg.css('filter', 'brightness(100%)');
-      secondCard = currentCard;
-    
-      // Sprawdzenie, czy odkryte karty są takie same
-      const secondImg = secondCard.children('img');
-      if (currentImg.attr('src') === secondImg.attr('src')) {
-        // Odkryte karty są takie same
-        setTimeout(() => {
-          currentCard.css('background-color', '');
-          currentImg.css('filter', '');
-          secondCard.css('background-color', '');
-          secondImg.css('filter', '');
-          // currentImg.attr('src', '');
-          // secondImg.attr('src', '');
-        }, 3000);
-      } else {
-        // Odkryte karty są różne
-        setTimeout(() => {
-          currentCard.css('background-color', '');
-          currentImg.css('filter', 'brightness(0%)');
-          secondCard.css('background-color', '');
-          secondImg.css('filter', 'brightness(0%)');
-        }, 3000);
+
+    if (!currentCard.hasClass('locked')) {
+      if (firstCard === null) {
+        // Odkrycie pierwszej karty
+        currentCard.css('background-color', 'white');
+        currentImg.css('filter', 'brightness(100%)');
+        firstCard = currentCard;
+        currentCard.addClass('locked');
+      } else if (secondCard === null) {
+        // Odkrycie drugiej karty
+        const firstImg = firstCard.children('img');
+        currentCard.css('background-color', 'white');
+        currentImg.css('filter', 'brightness(100%)');
+        secondCard = currentCard;
+        currentCard.addClass('locked');
+
+        if (currentImg.attr('src') === firstImg.attr('src')) {
+          // Karty takie same
+          currentImg.css('filter', 'brightness(50%)');
+          currentCard.css('background-color', 'grey')
+          firstImg.css('filter', 'brightness(50%)');
+          firstCard.css('background-color', 'grey')
+
+          firstCard = null;
+          secondCard = null;
+          //$('td.locked').removeClass('locked');
+        } else {
+          // Karty różne
+          setTimeout(() => {
+            currentCard.css('background-color', 'black');
+            currentImg.css('filter', 'brightness(0%)');
+            firstCard.css('background-color', 'black');
+            firstImg.css('filter', 'brightness(0%)');
+            firstCard = null;
+            secondCard = null;
+            $('td.locked').removeClass('locked');
+          }, 2000);
+        }
       }
-      firstCard = null;
-      secondCard = null;
     }
   });
 });
 
 
-// ---------------------0000000000000000000000000--------------------------------
-// Ten poniej tez fajny:
+__________________________________________________________________________________
+//to prawie to o co mi chodzi
 
 // $(document).ready(() => {
 //   let firstCard = null;
+//   let secondCard = null;
+//   let isChecking = false;
+
 //   $('td').click(function() {
+//     if (isChecking) {
+//       return;
+//     }
+    
 //     const currentCard = $(this);
 //     const currentImg = currentCard.children('img');
-    
+
 //     if (firstCard === null) {
 //       // Odkrycie pierwszej karty
 //       currentCard.css('background-color', 'white');
 //       currentImg.css('filter', 'brightness(100%)');
 //       firstCard = currentCard;
-//     } else {
+//     } else if (secondCard === null) {
 //       // Odkrycie drugiej karty
 //       const firstImg = firstCard.children('img');
 //       currentCard.css('background-color', 'white');
 //       currentImg.css('filter', 'brightness(100%)');
-      
+//       secondCard = currentCard;
+
+//       // Blokowanie kliknięć na kartach podczas sprawdzania, czy dwie karty są takie same
+//       isChecking = true;
+
+//       // Sprawdzenie, czy dwie karty są takie same
 //       if (currentImg.attr('src') === firstImg.attr('src')) {
-//         // Odkryte karty są takie same
+//         // Karty są takie same
 //         setTimeout(() => {
-//           currentCard.css('background-color', '');
-//           currentImg.css('filter', '');
-//           firstCard.css('background-color', '');
-//           firstImg.css('filter', '');
-//           currentImg.attr('src', '');
-//           firstImg.attr('src', '');
-//         }, 3000);
+//           currentCard.css('background-color', 'black');
+//           firstCard.css('background-color', 'black');
+//           currentImg.css('filter', 'brightness(0%)');
+//           firstImg.css('filter', 'brightness(0%)');
+//           firstCard = null;
+//           secondCard = null;
+
+//           // Odblokowanie kliknięć na kartach
+//           isChecking = false;
+//         }, 2000);
 //       } else {
-//         // Odkryte karty są różne
+//         // Karty nie są takie same
 //         setTimeout(() => {
-//           currentCard.css('background-color', '');
-//           currentImg.css('filter', '');
-//           firstCard.css('background-color', '');
-//           firstImg.css('filter', '');
-//         }, 3000);
+//           currentCard.css('background-color', 'black');
+//           firstCard.css('background-color', 'black');
+//           currentImg.css('filter', 'brightness(0%)');
+//           firstImg.css('filter', 'brightness(0%)');
+//           firstCard = null;
+//           secondCard = null;
+
+//           // Odblokowanie kliknięć na kartach
+//           isChecking = false;
+//         }, 2000);
 //       }
-//       firstCard = null;
 //     }
 //   });
 // });
-
-
-//-----------------------------------------------------------------------------------------------------------
-// - to co na GIT
-
-
-// $(document).ready(() => {
-//   let firstCard = null;
-//   let secondCard = null;
-//   $('td').each(function(index) {
-//     const td = $(this);
-//     td.click(() => {
-//       td.css('background-color', 'white');
-//       td.children('img').css('filter', 'brightness(100%)');
-
-//       if (!firstCard) {
-//         firstCard = td;
-//       } else {
-//         const firstCardName = firstCard.children('img').attr('alt');
-//         const secondCardName = td.children('img').attr('alt');
-//         if (firstCardName === secondCardName) {
-//           firstCard.children('img').attr('src', '');
-//           firstCard.css('background-color', 'white');
-//           td.children('img').attr('src', '');
-//           td.css('background-color', 'white');
-//         } else {
-//           setTimeout(() => {
-//             firstCard.css('background-color', '');
-//             firstCard.children('img').css('filter', '');
-//             td.css('background-color', '');
-//             td.children('img').css('filter', '');
-//           }, 3000);
-//         }
-//         firstCard = null;
-//         secondCard = null;
-//       }
-//     });
-//   });
-// });
-
-
-
-// koniec działającego kodu
-
-
