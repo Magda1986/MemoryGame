c8b2659705314d50e59e206053a001be8b6fb1bc
mratajczak <magda.ratajczak@viacon.pl>
2023-06-05 14:41:22 +0200

    fixing

--- Gra/models.py
+++ Gra/models.py
@@ -1,45 +1,33 @@
 from django.db import models
 from random import shuffle
 
-cards = [
-    (4, "4 cards"),
-    (8, "8 cards"),
-    (16, "16 cards"),
-    (32, "32 cards")
-    ]
+cards = [(4, "4 cards"), (8, "8 cards"), (16, "16 cards"), (32, "32 cards")]
 
 
 def create_playboard(cards_no, row_len=8):
-    deck = 2 * list(range(int(0.5 * cards_no)))  #tworzymy talie kart
-    shuffle(deck)# tasowanie
-    return [deck[i: i+row_len] for i in range(0, len(deck), row_len)] #ukladanie kart na planszy
+    deck = 2 * list(range(int(0.5 * cards_no)))  # tworzymy talie kart
+    shuffle(deck)  # tasowanie
+    return [deck[i : i + row_len] for i in range(0, len(deck), row_len)]  # ukladanie kart na planszy
 
 
 class NewGame(models.Model):
-    player1 = models.CharField(max_length = 15, blank=True)
-    player2 = models.CharField(max_length = 15, blank=True)
+    player1 = models.CharField(max_length=15, blank=True)
+    player2 = models.CharField(max_length=15, blank=True)
     number_cards = models.IntegerField(choices=cards)
-    playboard = models.JSONField(blank=True, null=True) #generuje sie jako pusty
-    pairs_total = models.IntegerField(blank=False, null=True)
-    moves = models.IntegerField(default=0) # pole liczące ruchy graczy
-    
+    playboard = models.JSONField(blank=True, null=True)  # generuje sie jako pusty
+    moves = models.IntegerField(default=0)  # pole liczące ruchy graczy
+
     def save(self, *args, **kwargs):
-        # self.playboard = create_playboard(self.number_cards)
-        # self.pairs_total = int((self.number_cards)/2)
+        if not self.playboard:
+            self.playboard = create_playboard(self.number_cards)
         super().save(*args, **kwargs)
 
+    # number_cards = models.DecimalField(default=10 , max_digits=20, decimal_places=0)
 
-
-
-    #number_cards = models.DecimalField(default=10 , max_digits=20, decimal_places=0)
-
-    #Nadpisywanie funkcji save do sprawdzeniam - do sprawdzenia. podczas zapisywania modelu tworzy się plansza!! W klasie name NADPISYWANIE FUNKCJI SAVE W DJANGO!! 
+    # Nadpisywanie funkcji save do sprawdzeniam - do sprawdzenia. podczas zapisywania modelu tworzy się plansza!! W klasie name NADPISYWANIE FUNKCJI SAVE W DJANGO!!
     # POLE PLANSZA
     # BLANCK
-    # w trakcie swywołania funkcji save w tworzy sie playground 
-    
-    
-    
+    # w trakcie swywołania funkcji save w tworzy sie playground
 
 
 # Create your models here.
--- Gra/views.py
+++ Gra/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import NewGameForm
-#from jinja2 import Template
-from random import shuffle
+
+# from jinja2 import Template
 from .models import NewGame, create_playboard
 from django.http import JsonResponse
 
@@ -10,69 +10,75 @@ def gra(request, *args, **kwargs):
     form = NewGameForm(request.POST or None)
     if form.is_valid():
         test = form.save()
-        test.playboard = create_playboard(test.number_cards)
-        test.pairs_total = int((test.number_cards)/2) 
-        test.save()
-        game=NewGame.objects.get(id=test.id)
         return redirect(f"rozgrywka/{test.id}/")
-    contex = {"nazwa" : "Gra", "form" : form, }
+    contex = {
+        "nazwa": "Gra",
+        "form": form,
+    }
     return render(request, "gra/gra.html", contex)
 
 
 def rozgrywka(request, id):
-    game=NewGame.objects.get(id=id)
-    context = {"cards": game.playboard, "id": id, "player1": game.player1, "player2": game.player2, "pairs_total": game.pairs_total}
+    game = NewGame.objects.get(id=id)
+    context = {
+        "cards": game.playboard,
+        "id": id,
+        "player1": game.player1,
+        "player2": game.player2,
+        "pairs_total": round(game.number_cards * 0.5),
+    }
     return render(request, "gra/rozgrywka.html", context)
 
 
 def moves(request, id):
-    if request.method == 'POST':
-        game = NewGame.objects.get(id=id)
-        moves = request.POST.get('moves')
-        print(moves) # wypisuje na konsoli wartość moves
-        game.moves = moves
-        game.save()
-        return JsonResponse({'success': True})
+    if request.method == "POST":
+        game = NewGame.objects.filter(id=id)
+        moves = request.POST.get("moves")
+        game.update(moves=moves)
+        return JsonResponse({"success": True})
     else:
-        return JsonResponse({'success': False})
+        return JsonResponse({"success": False})
 
 
 def wyniki(request):
-    # pobieramy wszystkie obiekty NewGame z bazy danych
-    wyniki = NewGame.objects.all()
-    # inicjujemy pustą listę, do której będziemy dodawać wyniki
-    wyniki_list = []
-    # przejdź przez każdy obiekt NewGame i dodaj go do listy wyników
-    for wynik in wyniki:
-        # pobierz nazwy graczy, liczbę par i liczbę ruchów dla każdej gry
-        player1 = wynik.player1
-        player2 = wynik.player2
-        pairs_total = wynik.pairs_total
-        moves = wynik.moves
-        if pairs_total is not None and moves is not None and moves !=0:
-        # dodajemy wyniki do listy wyników
-            wyniki_list.append({
-                'player1': player1,
-                'player2': player2,
-                'pairs_total': pairs_total,
-                'moves': moves
-            })
-    
-    context = {
-        'nazwa': 'Wyniki Gry',
-        'wyniki': wyniki_list
-    }
-    
+    # # pobieramy wszystkie obiekty NewGame z bazy danych
+    # wyniki = NewGame.objects.all()
+    # # inicjujemy pustą listę, do której będziemy dodawać wyniki
+    # wyniki_list = []
+    # # przejdź przez każdy obiekt NewGame i dodaj go do listy wyników
+    # for wynik in wyniki:
+    #     # pobierz nazwy graczy, liczbę par i liczbę ruchów dla każdej gry
+    #     if wynik.moves:
+    #         # dodajemy wyniki do listy wyników
+    #         wyniki_list.append(
+    #             {
+    #                 "id": wynik.id,
+    #                 "player1": wynik.player1,
+    #                 "player2": wynik.player2,
+    #                 "moves": wynik.moves,
+    #                 "pairs_total": round(wynik.number_cards * 0.5),
+    #             }
+    #         )
+
+    wyniki_list = [
+        {
+            "id": wynik.id,
+            "player1": wynik.player1,
+            "player2": wynik.player2,
+            "moves": wynik.moves,
+            "pairs_total": round(wynik.number_cards * 0.5),
+        }
+        for wynik in NewGame.objects.all()
+        if wynik.moves
+    ]
+
+    context = {"nazwa": "Wyniki Gry", "wyniki": wyniki_list}
+
     # przekazuję listę wyników do szablonu HTML
-    return render(request, 'gra/wyniki.html', context)
-    
+    return render(request, "gra/wyniki.html", context)
 
 
 # def create_playboard(cards_no, row_len=4):
 #     deck = 2 * list(range(int(0.5 * cards_no)))  #tworzymy talie kart
 #     shuffle(deck)# tasowanie
 #     return [deck[i: i+row_len] for i in range(0, len(deck), row_len)] #ukladanie kart na planszy
-
-
-
-
