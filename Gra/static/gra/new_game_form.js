610175946e35782e3ccf05c9558aee8a52d5bf2e
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2023-06-19 09:20:49 +0200

    fixed jQuery in new_game_form

--- Gra/static/gra/new_game_form.js
+++ Gra/static/gra/new_game_form.js
@@ -1,33 +1,15 @@
 
-$(document).ready(function() { 
+ $(document).ready(function() {
     $('#div_player2').hide();
+    
     $('#id_player2_select').change(function(){
         var selected = $(this).find("option:selected").attr('value'); 
-        // alert(selected);
-        // alert(selected==="player3");
-        if (selected==="player3"){
-            $('#div_player2').hide()
+        
+        if (selected === "player1") {
+            $('#div_player2').hide();
         } else {
-            $('#div_player2').show()
+            $('#div_player2').show();
         }
     });
- });
+});
 
-
-
- 
- // $(document).ready(function() {
-//     const player2Div = $('#div_player2');
-//     const player2Select = $('#id_player2_select');
-  
-//     player2Div.hide();
-  
-//     player2Select.change(function(){
-//       const selected = $(this).find("option:selected").attr('value'); 
-//       if (selected === "player3") {
-//         player2Div.hide();
-//       } else {
-//         player2Div.show();
-//       }
-//     });
-//   });
